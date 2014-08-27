6bdc58ee2fa813061d1681ed7325179106b2a098
apsillers <asillers@mitre.org>
2014-08-27 15:11:34 -0400

    Change `id` property to `id_`. Fixes #2.

--- anubis_parser.py
+++ anubis_parser.py
@@ -171,7 +171,7 @@ class parser:
         self.bundle_obj.set_process_tree(ProcessTree.from_dict(process_tree))
 
         malware_subject.add_findings_bundle(self.bundle_obj)
-        malware_subject.analyses[0].set_findings_bundle(self.bundle_obj.id)
+        malware_subject.analyses[0].set_findings_bundle(self.bundle_obj.id_)
         
         self.maec_subjects.append(malware_subject)
         
